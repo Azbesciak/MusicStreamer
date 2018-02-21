68068fcdc8e83871b3617c10b1b40b122470f887
Szymon Wolarz <szymon.wol@vp.pl>
2018-02-21 20:59:57 +0100

    Add experimental player implementation

--- frontend/src/main/kotlin/cs/sk/musicstreamer/room/MusicPlayer.kt
+++ frontend/src/main/kotlin/cs/sk/musicstreamer/room/MusicPlayer.kt
@@ -1,84 +1,66 @@
 package cs.sk.musicstreamer.room
 
-import cs.sk.musicstreamer.connection.connectors.StreamingConnector
 import cs.sk.musicstreamer.connection.connectors.StreamingListener
 import cs.sk.musicstreamer.connection.connectors.Switchable
+import mu.KLogging
 import org.springframework.stereotype.Service
-import javax.sound.sampled.AudioFormat
-import javax.swing.*
-import java.awt.*
-import java.awt.event.*
-import java.io.*
-import java.net.*
 import javax.sound.sampled.*
-import javax.sound.sampled.AudioSystem
-import javax.sound.sampled.SourceDataLine
-import javax.sound.sampled.DataLine
-import javax.sound.sampled.AudioInputStream
-import java.io.ByteArrayInputStream
-import java.net.DatagramPacket
-import java.net.DatagramSocket
-import javax.sound.sampled.TargetDataLine
-import java.io.ByteArrayOutputStream
 
 
 @Service
 class MusicPlayer {
-    private var audioFormat: AudioFormat? = null
-    private var targetDataLine: TargetDataLine? = null
-    private var byteOutputStream = ByteArrayOutputStream()
+
+    companion object: KLogging()
+
     private lateinit var provider: Switchable
+    private var audioLine: SourceDataLine? = null
+
+
     fun play() {
+        provider.start()
     }
+
 
     fun stop() {
-
+        provider.stop()
     }
 
+
     fun getStreamingListener() = StreamingListener(
-            onNewData = { audioData ->
-                val byteInputStream = ByteArrayInputStream(audioData)
-                val inputStream = AudioInputStream(byteInputStream, audioFormat, (audioData.size / audioFormat!!.frameSize).toLong())
-                val dataLineInfo = DataLine.Info(SourceDataLine::class.java, audioFormat)
-                val sourceLine = AudioSystem.getLine(dataLineInfo) as SourceDataLine
-                sourceLine.open(audioFormat)
-                sourceLine.start()
-            },
-            onError = {},
-            formatListener = { audioFormat = it },
-            switch = { provider = it }
+            formatListener = { audioFormat -> prepareNewTrack(audioFormat) },
+            onNewData = { audioData -> pushAudioData(audioData) },
+            switch = { provider = it },
+            onError = {}
     )
 
 
+    @Synchronized
+    fun prepareNewTrack(audioFormat: AudioFormat) {
+
+        logger.debug("Received audio format of a new track\n$audioFormat")
+        audioLine?.close()
+
+        audioLine = AudioSystem.getLine(
+                DataLine.Info(SourceDataLine::class.java, audioFormat)) as SourceDataLine
+
+        audioLine?.open(audioFormat)
+        audioLine?.start()
+    }
+
+    @Synchronized
+    fun pushAudioData(audioData: ByteArray) {
+
+        logger.debug("Received ${audioData.size} audio bytes...")
+        audioLine?.write(audioData, 0, audioData.size)
+    }
 }
 
 //
 //class VUServer {
 //
-//    internal var byteOutputStream: ByteArrayOutputStream? = null
-//    internal var adFormat: AudioFormat? = null
-//    internal var targetDataLine: TargetDataLine? = null
 //    internal var InputStream: AudioInputStream
 //    internal var sourceLine: SourceDataLine
 //
-//    private val audioFormat: AudioFormat
-//        get() {
-//            val sampleRate = 16000.0f
-//            val sampleInbits = 16
-//            val channels = 1
-//            val signed = true
-//            val bigEndian = false
-//            return AudioFormat(sampleRate, sampleInbits, channels, signed, bigEndian)
-//        }
-//
-//    fun runVOIP() {
-//        try {
-//            val serverSocket = DatagramSocket(9786)
-//            val receiveData = ByteArray(10000)
-//            while (true) {
-//                val receivePacket = DatagramPacket(receiveData, receiveData.size)
-//                serverSocket.receive(receivePacket)
-//                println("RECEIVED: " + receivePacket.address.hostAddress + " " + receivePacket.port)
 //                try {
 //                    val audioData = receivePacket.data
 //                    val byteInputStream = ByteArrayInputStream(audioData)
